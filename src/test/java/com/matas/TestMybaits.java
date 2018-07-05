95526e6188941be11f2dd2cba794f4fe61a82b29
mataszhang <mataszhang@163.com>
2018-07-05 18:01:36 +0800

    mybaits

--- src/test/java/com/matas/TestMybaits.java
+++ src/test/java/com/matas/TestMybaits.java
@@ -6,10 +6,25 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.PropertyResourceConfigurer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.util.ClassUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
 
 /**
  * @author matas
@@ -66,5 +81,57 @@ public class TestMybaits {
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         System.out.println(mapper.queryUserList());
         sqlSession.close();
+    }
+
+    @Test
+    public void queryUserList2() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("test-mybatis.xml");
+        UserMapper mapper = ctx.getBean(UserMapper.class);
+        System.out.println(mapper.queryUserList());
+    }
+
+    /**
+     * 测试processPropertyPlaceHolders属性的作用
+     * <p>提前解析basePackage配置的PropertyPlaceHolder。</p>
+     * <p>
+     * 因为{@link PropertyResourceConfigurer }实现了{@link BeanFactoryPostProcessor }接口
+     * 意味着在BeanFactory加载完所有的BeanDefinition后才会调用。而{@link MapperScannerConfigurer}在扫描Mapper的时候，需要替换配置的属性。
+     * 所以需要通过processPropertyPlaceHolders来控制是否提前解析PropertyPlaceHolder。通过模拟spirng的环境，来调用PropertyResourceConfigurer解析配置
+     * </p>
+     *
+     * @param
+     * @return
+     * @author matas
+     * @date 2018/7/5 16:40
+     * @see MapperScannerConfigurer#processPropertyPlaceHolders()
+     */
+    @Test
+    public void testBasePackage() {
+        //applicationContext instanceof GenericApplicationContext
+        ApplicationContext ctx = new GenericXmlApplicationContext("test-mybatis.xml");
+        UserMapper mapper = ctx.getBean(UserMapper.class);
+        System.out.println(mapper.queryUserList());
+    }
+
+    @Test
+    public void testScan() throws Exception {
+        String basePackage = "com.matas.aop";
+        String convertPath = ClassUtils.convertClassNameToResourcePath(basePackage); // 包名装路径
+        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +
+                convertPath + "/" + "**/*.class";
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);//路径下所有的*.class
+        System.out.println(resources.length);
+
+
+        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(true);
+        provider.addIncludeFilter((new TypeFilter() {
+            @Override
+            public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
+                return true;
+            }
+        })); //所有的类都匹配
+        Set<BeanDefinition> candidateComponents = provider.findCandidateComponents(basePackage);
+        System.out.println(candidateComponents.size());
     }
 }
